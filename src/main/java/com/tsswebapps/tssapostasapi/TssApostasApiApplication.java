92aff042f56958c32785854a7b22a5db756acb02
xthiagosant <uzmaki_thiago@outllok.com>
2021-12-30 11:42:25 -0300

    primeiro commit

---
+++ src/main/java/com/tsswebapps/tssapostasapi/TssApostasApiApplication.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.tssapostasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TssApostasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TssApostasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsswebapps/tssapostasapi/TssApostasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsswebapps.tssapostasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TssApostasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
